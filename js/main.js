81e206db70c15c3a4aadc53f6607ce3d06179ef0
Danish Rasheed <danish.rasheed@regbits.com>
2021-10-08 11:30:34 +0500

    Add repo

---
+++ js/main.js
@@ -0,0 +1,21 @@
+function myfunction(){
+  document.querySelector('.navbar-nav').classList.toggle('display-block')
+  document.querySelector('.nav-buttons').classList.toggle('display-flex')
+}
+
+function toggleDashboardNav(){
+  document.querySelector('.nav-searchbar').classList.toggle('display-flex')
+
+}
+
+function toggleDashboardSidebar(){
+    document.querySelector('.side-bar').classList.toggle('display-flex')
+
+}
+    
+
+function toggleDashboardrightBar(){
+    document.querySelector('.main-section-right').classList.toggle('display-flex')
+
+}
+
